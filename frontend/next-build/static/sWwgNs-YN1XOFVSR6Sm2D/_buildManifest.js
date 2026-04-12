9b87dae6a2dded9398ff4a778580d84be2ff0f9e
ayush <ayushchanekar64@gmail.com>
2026-04-12 20:24:18 +0530

    changes

---
+++ frontend/next-build/static/sWwgNs-YN1XOFVSR6Sm2D/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/973-0c8961eab1b69692.js","static/chunks/pages/index-2d22b22cf84b62fa.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
